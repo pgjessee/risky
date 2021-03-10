52f34b7b5dfcc75e345c4e63506ee1be547ff31a
pgjessee <peter.jessee@gmail.com>
2021-03-09 19:25:34 -0600

    user model complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,29 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
+
+
+from .config import Config
+from .models import *
+
 
 app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+app.config.from_object(Config)
+
+
+
+CORS(app)
---
+++ app/models/user.py
@@ -0,0 +1,33 @@
+from .db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    hashed_password = db.Column(db.String, nullable=False)
+    zip = db.Column(db.String(5), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+        }
