eae64daa9c7035b9ba5cd6ec940221404563e787
pgjessee <peter.jessee@gmail.com>
2021-03-14 21:19:38 -0500

    Added a request method handler for product

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, jsonify, session, request
+from flask import Blueprint, jsonify, session, request, make_response
 import requests
 
 API_KEY = os.environ.get('API_KEY')
@@ -8,9 +8,18 @@ products_routes = Blueprint('products', __name__)
 
 @products_routes.route('/<int:id>', methods=['GET'])
 def get_product(id):
-    print(API_KEY)
-    url = f"https://openapi.etsy.com/v2/listings/{id}?api_key={API_KEY}"
-    res = requests.get(url)
-    return res.json()
+
+    req_method = request.method
+
+    if req_method == "GET":
+        url = f"https://openapi.etsy.com/v2/listings/{id}?api_key={API_KEY}"
+        res = requests.get(url)
+        res = res.json()
+        res = res["results"][0]
+        return res
+
+    if req_method == "POST":
+        pass
+
 
 
